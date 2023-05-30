5cc0ef085097fb681dbfa1a866c97b08501ba947
Nyamadi Atsu <devbrainy98@gmail.com>
2023-05-30 03:59:50 +0100

    added logout route

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors({
-    origin: ['https://zedblock-mern.vercel.app/', 'http://localhost:3000/'],
+    origin: "*",
     credentials: true
 }));
 
