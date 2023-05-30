94b655d0f4a8a37ffa19e0708fa717725523261c
Nyamadi Atsu <devbrainy98@gmail.com>
2023-05-30 04:51:38 +0100

    cors workaround

--- server/index.js
+++ server/index.js
@@ -20,7 +20,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  credentials: true
+}));
 
 //connect to db
 connectDB();
