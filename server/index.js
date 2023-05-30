b68b1bdb28b76dcae8803848545c506e3ec335d7
Nyamadi Atsu <devbrainy98@gmail.com>
2023-05-30 05:10:16 +0100

    cors workaround

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: "https://zedblock-mern.vercel.app",
+  origin: ["https://zedblock-mern.vercel.app", 'http://localhost:3000'],
 }));
 
 //connect to db
--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -5,7 +5,7 @@ import { authenticateUser } from "../middleware/authUser.js";
 const router = express.Router();
 router.post('/login', login);
 router.post('/register', register);
-router.delete('/', authenticateUser, removeUser);
 router.post('/logout', authenticateUser, logout);
+router.delete('/', authenticateUser, removeUser);
 
 export default router;
